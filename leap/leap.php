0a19fcf86d4a96629e5c35bc58e533d4ab147c06
Alan Anderson <alan.anderson@udacity.com>
2019-08-06 14:48:31 -0700

    Add completed leap exercise

---
+++ leap/leap.php
@@ -0,0 +1,11 @@
+<?php
+
+function isLeap(int $year){
+    if ($year % 100 == 0 && $year % 400 != 0){
+        return false;
+    }
+    if ($year % 4 == 0){
+        return true;
+    }
+    return false;
+}
